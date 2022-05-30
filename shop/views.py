07954910942c698a60c6a61bf2dc2c45edb95e3c
kdmon7 <kshgreen2@naver.com>
2022-05-30 15:02:32 +0900

    220530kjh

--- shop/views.py
+++ shop/views.py
@@ -4,11 +4,13 @@ from .models import Post
 
 
 def post_view(request):
-    if request.method == 'POST':
+    #POST 방시
+    if request.method == 'POST': 
         post = Post()
         post.post_id = request.POST['post_id']
         post.save()
         redirect('post')
+    #GET 방식
     else:
         post = Post.objects.all()
         return render(request, 'index.html', {'post': post})
